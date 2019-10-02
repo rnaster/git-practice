6362e6a505f0562f6850d919c0d4adea4ace4b87
lys <blpomspo@gmail.com>
2019-10-03 04:18:41 +0900

    made other changes

--- ex0.py
+++ ex0.py
@@ -3,3 +3,13 @@ import random
 
 if __name__ == '__main__':
     print('print random number: %d' % random.randint(0, 100))
+    rand = random.random()
+    if rand < 0.3:
+        print('not bad')
+    elif rand < 0.6:
+        print('good')
+    elif rand < 0.9:
+        print('great')
+    else:
+        print('excellent')
+
