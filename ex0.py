7cbb5b68f90a4b695aa0dd4aaad5f4eba1812ea9
lys <blpomspo@gmail.com>
2019-10-04 01:33:28 +0900

    add random number list

--- ex0.py
+++ ex0.py
@@ -20,3 +20,5 @@ if __name__ == '__main__':
     rand_list = [random.randint(0, 20) for _ in range(5)]
     print('origin:', *rand_list)
     print('power:', *map(power, rand_list))
+    print('random number list')
+    print(*[round(random.random(), 4) for _ in range(10)])
